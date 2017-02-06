df6c3a51346f4ea03b19d48c31ba36767c217b2d
DrCopernicus <nicholas.copernicus.lee@gmail.com>
2017-02-05 19:04:42 -0700

    linked list

---
+++ LotsOfPracticeProblems/LinkedListReversal.cs
@@ -0,0 +1,35 @@
+﻿namespace LotsOfPracticeProblems
+{
+    public class LinkedNode
+    {
+        public LinkedNode(int value)
+        {
+            Value = value;
+        }
+
+        public LinkedNode Next;
+        public int Value;
+    }
+
+    public class LinkedListReversal
+    {
+        public LinkedNode BumperCartsReverse(LinkedNode head)
+        {
+            LinkedNode previous = null;
+            var current = head;
+            var next = current.Next;
+
+            while (next != null)
+            {
+                current.Next = previous;
+                previous = current;
+                current = next;
+                next = current.Next;
+            }
+
+            current.Next = previous;
+
+            return current;
+        }
+    }
+}
---
+++ Tests/LinkedListReversalTests.cs
@@ -0,0 +1,50 @@
+﻿using LotsOfPracticeProblems;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LinkedListReversalTests
+    {
+        [Test]
+        public void OneNodeReversal()
+        {
+            var head = new LinkedNode(0);
+
+            var reversed = new LinkedListReversal().BumperCartsReverse(head);
+
+            Assert.AreEqual(0, reversed.Value);
+            Assert.AreEqual(null, reversed.Next);
+        }
+
+        [Test]
+        public void SmallListReversal()
+        {
+            var head = new LinkedNode(0);
+            head.Next = new LinkedNode(1);
+
+            var reversed = new LinkedListReversal().BumperCartsReverse(head);
+
+            Assert.AreEqual(1, reversed.Value);
+            Assert.AreEqual(0, reversed.Next.Value);
+            Assert.AreEqual(null, reversed.Next.Next);
+        }
+
+        [Test]
+        public void BiggerListReversal()
+        {
+            var head = new LinkedNode(0);
+            head.Next = new LinkedNode(1);
+            head.Next.Next = new LinkedNode(2);
+            head.Next.Next.Next = new LinkedNode(3);
+
+            var reversed = new LinkedListReversal().BumperCartsReverse(head);
+
+            Assert.AreEqual(3, reversed.Value);
+            Assert.AreEqual(2, reversed.Next.Value);
+            Assert.AreEqual(1, reversed.Next.Next.Value);
+            Assert.AreEqual(0, reversed.Next.Next.Next.Value);
+            Assert.AreEqual(null, reversed.Next.Next.Next.Next);
+        }
+    }
+}
