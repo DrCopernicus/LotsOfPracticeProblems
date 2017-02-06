ce3e2f8456e3480a4b0c8be44172ca1bee4278f5
DrCopernicus <nicholas.copernicus.lee@gmail.com>
2017-02-05 19:47:49 -0700

    apple stock trading problem

---
+++ LotsOfPracticeProblems/AppleStocksTrading.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LotsOfPracticeProblems
+{
+    public class AppleStocksTrading
+    {
+        public int BruteForceProfit(int[] priceHistory)
+        {
+            if (priceHistory.Length < 2)
+                throw new Exception("blah");
+
+            var max = int.MinValue;
+            for (var i = 0; i < priceHistory.Length - 1; i++)
+            {
+                for (var j = i + 1; j < priceHistory.Length; j++)
+                {
+                    var difference = priceHistory[j] - priceHistory[i];
+                    if (difference > max)
+                        max = difference;
+                }
+            }
+            return max;
+        }
+
+        public int ProfitByScrolling(int[] priceHistory)
+        {
+            if (priceHistory.Length < 2)
+                throw new Exception("blah");
+
+            var minPrice = priceHistory[0];
+            var maxProfit = int.MinValue;
+
+            for (var i = 1; i < priceHistory.Length; i++)
+            {
+                maxProfit = Math.Max(maxProfit, priceHistory[i] - minPrice);
+                minPrice = Math.Min(minPrice, priceHistory[i]);
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ Tests/AppleStocksTradingTests.cs
@@ -0,0 +1,36 @@
+﻿using LotsOfPracticeProblems;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AppleStocksTradingTests
+    {
+        [Test]
+        public void AlwaysNegative()
+        {
+            var trade = new AppleStocksTrading();
+            Assert.AreEqual(-1, trade.ProfitByScrolling(new[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }));
+            Assert.AreEqual(-2, trade.ProfitByScrolling(new[] { 10, 8 }));
+            Assert.AreEqual(-2, trade.ProfitByScrolling(new[] { 10, 8, 5 }));
+            Assert.AreEqual(-1, trade.ProfitByScrolling(new[] { 10, 5, 4 }));
+            Assert.AreEqual(-1, trade.ProfitByScrolling(new[] { 10, 9, 4 }));
+            Assert.AreEqual(0, trade.ProfitByScrolling(new[] { 10, 10 }));
+            Assert.AreEqual(0, trade.ProfitByScrolling(new[] { 0, 0 }));
+        }
+
+        [Test]
+        public void AlwaysPositive()
+        {
+            var trade = new AppleStocksTrading();
+            Assert.AreEqual(7, trade.ProfitByScrolling(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }));
+        }
+
+        [Test]
+        public void Messy()
+        {
+            var trade = new AppleStocksTrading();
+            Assert.AreEqual(6, trade.ProfitByScrolling(new[] { 5,3,7,9,4,5,6,6,4,5,7,5,6,7,4,6,2,6,3,1,5}));
+        }
+    }
+}
